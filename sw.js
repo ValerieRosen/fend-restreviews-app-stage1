698f851f48f8764a9c9a98f2b2fb293d54d14d92
Valerie <vrosenhauer@gmail.com>
2018-12-18 00:23:49 -0500

    initial commit

---
+++ sw.js
@@ -0,0 +1,68 @@
+console.log('Service Worker: Registered');
+
+const cacheName = 'version1';
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('version1').then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+let staticCacheName = 'restaurant-static-v1';
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+			return cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                '/css/styles.css',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                './js/sw_register.js',
+                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+                '/data/restaurants,json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',  
+			]);
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys()
+		.then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('restaurant-') &&
+						   cacheName != staticCacheName;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+})
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
