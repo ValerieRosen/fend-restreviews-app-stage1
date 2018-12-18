50f379d7c8bda1b681f22875322edf2886f72f89
Valerie <vrosenhauer@gmail.com>
2018-12-18 00:23:30 -0500

    add sw script

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -179,3 +179,15 @@ class DBHelper {
 
 }
 
+// Register a service worker hosted at the root of the
+// site using the default scope.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }).catch(function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
+
